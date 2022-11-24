4cb6334b04933ec41734b9ebf7210bbc078f6185
Danisharshad123 <danisharshad976@gmail.com>
2022-11-24 14:39:11 +0500

    add

---
+++ index.js
@@ -0,0 +1,17 @@
+/* function JavaScript() {
+
+    document.getElementById('demo').innerHTML = "this is working";
+    
+} */
+
+
+/* let x,y,z;
+
+x=4;
+y=6;
+z = x + y;
+
+function myFunction() {
+    document.getElementById('demo').innerHTML = "The value of z is " + z + ".";
+} */
+
